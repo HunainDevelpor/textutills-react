57e5ab498cf4c68d387621be452ef17c48339a8a
HunainDevelpor <hunainkhattak0342@gmail.com>
2024-01-24 13:20:27 +0500

    Changes

--- src/Alert.js
+++ src/Alert.js
@@ -1,13 +1,13 @@
 import React from "react";
-import PropTypes from 'prop-types';
+
 
 export default function Alert(props){
 return(
 
-    
-   props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <div style={{height:'50px'}}>
+   {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
   <strong>{props.alert.type}</strong>:  {props.alert.msg} 
-  
+  </div>}
 </div>
 
     
--- src/Textutills.js
+++ src/Textutills.js
@@ -8,7 +8,7 @@ function Textutills(props){
   const Upperconvert = () => {
     let newtext = text.toUpperCase();
     setText(newtext); 
-    props.showAlert("converted successfully to UpperCase ","success")
+    {text.length!==0&&  props.showAlert("converted successfully to UpperCase ","success")}
 }
 
 const change = (event) => {
@@ -17,23 +17,24 @@ const change = (event) => {
 }
 const clear = () => {
     setText("");
-    props.showAlert("cleared successfully ","success")
+    {text.length!==0&& props.showAlert("cleared successfully ","success")}
+
 }
 const [text, setText] = useState("");
 
 const copytext=()=>{
   let textarea =document.getElementById("exampleFormControlTextarea1");
-  textarea.rselect();
+  textarea.select();
   textarea.setSelectionRange(0,9999);
   navigator.clipboard.writeText(textarea.value);
-  props.showAlert("Copy text successfully ","success")
+  {text.length!==0&&props.showAlert("Copy text successfully ","success")}
 }
 
 const Removespaces=()=>{
 
   let newtext = text.split(/[ ]+/);
   setText(newtext.join(" "));
-  props.showAlert("Extra Space removed successfully ","success")
+  {text.length!==0&&props.showAlert("Extra Space removed successfully ","success")}
 }
 
 
@@ -51,11 +52,14 @@ return(
   <label for="exampleFormControlTextarea1" className="form-label">Textarea</label>
   <textarea className={`form-control bg-${props.mode==='light'?'light':'secondary'} text-${props.mode==='light'?'dark':'light'}  `} value={text} id="exampleFormControlTextarea1" onChange={change} rows="7"></textarea>
   <div className="container my-3">
-  <div onClick={Upperconvert}className="btn btn-secondary mx-2">UpperCase</div>
-  <div onClick={clear} className="btn btn-secondary mx-2">Clear</div>
-  <div onClick={copytext} className="btn btn-secondary mx-2">Copy</div>
-  <div onClick={Removespaces} className="btn btn-secondary mx-2">Remove Extra Spaces</div>
-  {/* <div onClick={toggle} className="btn btn-secondary mx-2">mode</div> */}
+  <div onClick={Upperconvert}className="btn btn-secondary my-2 mx-2">UpperCase</div>
+  <div onClick={clear} className="btn btn-secondary mx-2 my-2">Clear</div>
+  <div onClick={copytext} className="btn btn-secondary mx-2 my-2">Copy</div>
+  <div onClick={Removespaces} className="btn btn-secondary mx-2 my-2">Remove Extra Spaces</div>
+  <div className="container my-3">
+                    <h3>Your Text Summary</h3>
+                    <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} Characters</p>
+                </div>
 
 
   </div>
